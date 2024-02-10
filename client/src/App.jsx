5fcd8a0f0b08a26e4b7cbee96077ff522e8052f1
indianpkm <iaspkmaurya@gmail.com>
2024-02-10 06:07:00 -0500

    fix react router issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,62 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  const handleClick = async () => {
-    try {
-      const res = await fetch('http://localhost:8080/test', {
-        method: 'GET',
-        headers: { 'content-type': 'application/json' },
-      });
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import { MyOtherComponent } from "./MyOtherComponent";
+import Home from "./Home";
   
-      if (!res.ok) {
-        throw new Error('Network response was not ok');
-      }
   
-      const data = await res.json();
-      console.log(data);
-      alert(data.message)
-  
-    } catch (error) {
-      console.error('Error fetching data:', error);
+  const router = createBrowserRouter([
+    {
+      path: "/my",
+      element: <MyOtherComponent/>,
+    }, 
+    {
+      path: "/",
+      element: <Home/>,
     }
-  };
+  ]);
   
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        
-      <button onClick={handleClick}>
-          Click here for see server response ✔✔🐱
-        </button>
-
-
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+  function App() {
+  
+    return (
+      <>
+      
+      <RouterProvider router={router} />
+  
+      </>
+    )
+  }
+  
+  export default App
+  
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 import express from "express";
+import { fileURLToPath } from 'url'; 
+import path from "path";
 const app = express();
+const port = process.env.PORT || 8000;
+
 app.use(express.json());
 
-app.use(express.static('client/dist'));
-const port = process.env.PORT || 8080;
+// Serve static files from 'client/dist' directory for all routes
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Define __dirname
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
+// Route handler for '/test'
 app.get('/test', (req, res) => {
   res.json({ message: 'This is a test response from the server 🐱🤷‍♀️' });
 });
 
-
+// Route handler for '/other'
 app.get('/other', (req, res) => {
-    res.json({ message: 'This is from other api 👍' });
-  });
+  res.json({ message: 'This is from other api 👍' });
+});
 
+// Wildcard route handler to serve index.html for all unmatched routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`server start at port ${port} `);
+  console.log(`Server started at port ${port}`);
 });
